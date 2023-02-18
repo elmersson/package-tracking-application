7731145ca4226d1c37c308a878a5906d9b1734a1
elmersson <elmerssonrasmus@gmail.com>
2023-02-18 14:35:05 +0100

    Install BottomTabNavigator

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,5 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
