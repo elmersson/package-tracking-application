12b07684691f7e82e2f77db0dbaac33c5e4c21b5
elmersson <elmerssonrasmus@gmail.com>
2023-02-18 14:18:17 +0100

    add react-navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {TailwindProvider} from 'tailwind-rn';
 import CustomersScreen from './screens/CustomersScreen';
 import utilities from './tailwind.json';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
     // @ts-ignore
     <TailwindProvider utilities={utilities}>
-      <CustomersScreen />
+      <NavigationContainer>
+        <CustomersScreen />
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -2,6 +2,7 @@ import { Text, SafeAreaView } from 'react-native'
 import React from 'react'
 import {useTailwind} from 'tailwind-rn';
 
+
 const CustomersScreen = () => {
     const tw = useTailwind();
   return (
