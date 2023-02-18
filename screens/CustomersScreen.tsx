2d6945d7c1f248d6e2598030b121bf463a97a700
elmersson <elmerssonrasmus@gmail.com>
2023-02-18 19:46:19 +0100

    implement type for customer

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,14 +1,36 @@
-import { Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { Text, SafeAreaView, ScrollView, ActivityIndicator} from 'react-native'
+import React, {useLayoutEffect} from 'react'
 import {useTailwind} from 'tailwind-rn';
+import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { TabStackParamList } from '../navigator/TabNavigator';
+import { RootStackParamList } from '../navigator/RootNavigatior';
+import { NativeStackNavigationProp } from "@react-navigation/native-stack"
+import { Image } from "@rneui/themed"
 
+export type CustomerScreenNavigationProp = CompositeNavigationProp<
+BottomTabNavigationProp<TabStackParamList, 'Customers'>,
+NativeStackNavigationProp<RootStackParamList>
+>
 
 const CustomersScreen = () => {
     const tw = useTailwind();
+    const navigation = useNavigation<CustomerScreenNavigationProp>();
+
+    useLayoutEffect(() => {
+      navigation.setOptions({
+        headerShown: false,
+      });
+    }, [])
+
   return (
-    <SafeAreaView>
-      
-    </SafeAreaView>
+    <ScrollView style={{backgroundColor: "#8c9191"}}>
+      <Image 
+        source={{uri: "https://images.pexels.com/photos/5025669/pexels-photo-5025669.jpeg"}}
+        containerStyle={tw("w-full h-64")}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+    </ScrollView>
   )
 }
 
