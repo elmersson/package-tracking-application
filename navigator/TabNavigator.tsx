f879867ed9cfb35c81d6a383431df208784b43d7
elmersson <elmerssonrasmus@gmail.com>
2023-02-18 18:02:54 +0100

    implement icons in tab

--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -23,7 +23,27 @@ const TabNavigator = () => {
 
   return (
     <Tab.Navigator screenOptions={({route}) => ({
-        
+        tabBarActiveTintColor: "#59C1CC",
+        tabBarInactiveTintColor: "gray",
+        tabBarIcon: ({ focused, color, size}) => {
+            if(route.name === 'Customers') {
+                return (
+                    <Icon 
+                        name="users" 
+                        type="entypo"
+                        color={focused ? "#59C1CC" : "gray"}
+                    />
+                );
+            } else if (route.name === 'Orders') {
+                return (
+                    <Icon 
+                        name="box" 
+                        type="entypo"
+                        color={focused ? "#cc5972" : "gray"}
+                    />
+                );
+            }
+        }
     })}>
       <Tab.Screen name="Customers" component={CustomersScreen} /> 
       <Tab.Screen name="Orders" component={OrdersScreen} /> 
