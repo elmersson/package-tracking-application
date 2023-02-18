4f0046510e39bed7a538dddf3b557fbc78f49ad2
elmersson <elmerssonrasmus@gmail.com>
2023-02-18 14:27:00 +0100

    implement navigator

---
+++ navigator/RootNavigatior.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabNavigator from './TabNavigator';
+
+
+const RootStack = createNativeStackNavigator();
+
+const RootNavigatior = () => {
+  return (
+    <RootStack.Navigator>
+      <RootStack.Group>
+        <RootStack.Screen name="Main" component={TabNavigator} />
+      </RootStack.Group>
+    </RootStack.Navigator>
+  )
+}
+
+export default RootNavigatior
