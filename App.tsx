2a40e3136715064301b8e80f931fde178d7aa7c7
elmersson <elmerssonrasmus@gmail.com>
2023-02-18 21:36:31 +0100

    add apollo

--- App.tsx
+++ App.tsx
@@ -2,7 +2,13 @@ import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigatior from './navigator/RootNavigatior';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
+
+const client = new ApolloClient({
+  uri: 'https://dashboard.stepzen.com/explorer?endpoint=api%2Fjazzy-mongoose',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
