9faf0e7a57a12dd5122e9042873d93a073c88aca
elmersson <elmerssonrasmus@gmail.com>
2023-02-19 15:18:59 +0100

    create custom hook and type for it

---
+++ hooks/useCustomersOrders.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+
+import { GET_ORDERS } from "../graphql/queries";
+
+function useCustomersOrders(userId: any) {
+    const { loading, error, data } = useQuery(GET_ORDERS);
+    const [ orders, setOrders ] = useState<Order[]>([]);
+
+    useEffect(() => {
+        if ( data! ) return;
+
+        const orders: Order[] = data.getOrders.map(({value}: OrderResponse) => ({
+            carrier: value.carrier,
+            createdAt: value.createdAt,
+            shippingCost: value.shippingCost,
+            trackingId: value.trackingId,
+            trackingitems: value.trackingitems,
+            Address: value.Address,
+            City: value.City,
+            Lat: value.Lat,
+            Lng: value.Lng,
+
+        }));
+
+        const customerOrders = orders.filter(
+            (order) => order.trackingitems.customer_id === userId
+          );
+      
+          setOrders(customerOrders);
+        }, [data, userId]);
+      
+        return { loading, error, orders };
+    }
+      
+export default useCustomersOrders;
